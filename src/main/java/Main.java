0df3a10f1742c700ca3b46e23dda5da40f4a8417
Fredrik Carlsen <fredrik@carlsen.io>
2014-12-16 13:48:48 +0100

    16 dec 2014

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -20,7 +20,6 @@ public class Main {
 
     public static void main(String... args) {
 
-
         String neoLocation = "/Users/frecar/code/frecar/obelix/graph.db";
         GraphDatabaseService graphDb = new GraphDatabaseFactory()
                 .newEmbeddedDatabaseBuilder(neoLocation)
--- src/main/java/WebInterface.java
+++ src/main/java/WebInterface.java
@@ -1,10 +1,15 @@
+import com.google.gson.JsonObject;
 import events.NeoHelpers;
 import org.neo4j.graphdb.*;
 import org.neo4j.graphdb.traversal.Evaluators;
 import org.neo4j.tooling.GlobalGraphOperations;
+import spark.Filter;
+import spark.Request;
+import spark.Response;
 
 import java.util.*;
 
+import static spark.Spark.before;
 import static spark.Spark.get;
 
 public class WebInterface implements Runnable {
@@ -17,6 +22,7 @@ public class WebInterface implements Runnable {
         this.graphDb = graphdb;
     }
 
+
     public List<Long> getAllUsers() {
 
         Label userLabel = DynamicLabel.label("User");
@@ -28,8 +34,14 @@ public class WebInterface implements Runnable {
 
             List<Long> nodes = new ArrayList<>();
 
+
             for (Node node : users) {
                 nodes.add((long) node.getProperty("id_user"));
+                int c = 0;
+                for (Relationship n : node.getRelationships()) {
+                    c += 1;
+                }
+                System.out.println("User: " + node.getProperty("id_user") + " has read " + c + " docments");
             }
 
             System.out.println("Total users: " + nodes.size());
@@ -90,12 +102,127 @@ public class WebInterface implements Runnable {
         }
     }
 
+    public List<JsonObject> getAllRelationshipsTuples() {
+
+        Iterable<Relationship> users;
+
+        try (Transaction tx = graphDb.beginTx()) {
+            users = GlobalGraphOperations.at(graphDb).getAllRelationships();
+            tx.success();
+
+            List<JsonObject> relations = new ArrayList<>();
+
+            for (Relationship node : users) {
+
+                JsonObject obj = new JsonObject();
+
+                obj.addProperty("source", "user_" + node.getStartNode().getProperty("id_user"));
+                obj.addProperty("target", "record_" + node.getEndNode().getProperty("id_bibrec"));
+
+                relations.add(obj);
+            }
+
+            System.out.println("Total relations: " + relations.size());
+
+            return relations;
+        }
+    }
+
     public Map<Long, Long> getAllRelationships() {
         return getAllRelationships(null);
     }
 
     private double getLevelScore(int currentDepth, int maxDepth) {
-        return 1+(maxDepth/(currentDepth+1));
+        return 1 + (maxDepth / (currentDepth + 1));
+    }
+
+    public List<JsonObject> userRelationships(String userID, String depth) {
+        //        {source: "Microsoft", target: "Amazon", type: "licensing"},
+
+        int count = 0;
+
+        try (Transaction tx = graphDb.beginTx()) {
+            System.out.println(userID);
+            Node user = NeoHelpers.getOrCreateUserNode(graphDb, Long.parseLong(userID));
+
+            List<JsonObject> result = new ArrayList<>();
+
+            Map<String, ArrayList<String>> user_records = new HashMap<>();
+            Map<String, ArrayList<String>> record_users = new HashMap<>();
+
+            Map<String, Boolean> added = new HashMap<>();
+
+            String output = "";
+            for (Path position : graphDb.traversalDescription()
+                    .depthFirst()
+                    .relationships(NeoHelpers.RelTypes.DOWNLOADED)
+                            //.evaluator(Evaluators.fromDepth(3))
+                    .evaluator(Evaluators.toDepth(Integer.parseInt(depth)))
+                    .traverse(user)) {
+                output += position + "\n";
+
+                int c = 0;
+
+                for (Relationship rel : position.relationships()) {
+
+
+                    String id_user = rel.getStartNode().getProperty("id_user").toString();
+                    String id_bibrec = rel.getEndNode().getProperty("id_bibrec").toString();
+
+                    if (!user_records.containsKey(id_user)) {
+                        user_records.put(id_user, new ArrayList<>());
+                    }
+
+                    if (!user_records.get(id_user).contains(id_bibrec)) {
+                        user_records.get(id_user).add(id_bibrec);
+                    }
+
+                    if (!record_users.containsKey(id_bibrec)) {
+                        record_users.put(id_bibrec, new ArrayList<>());
+                    }
+
+                    if (!record_users.get(id_bibrec).contains(id_user)) {
+                        record_users.get(id_bibrec).add(id_user);
+                    }
+
+                }
+
+                for (Map.Entry<String, ArrayList<String>> entry : user_records.entrySet()) {
+
+                    if (entry.getValue().size() > 0) {
+
+                        for (String recid : entry.getValue()) {
+
+                            if (record_users.get(recid).size() > 0) {
+
+                                /*if (added.containsKey(entry.getKey()+recid)) {
+                                    continue;
+                                }*/
+
+                                count += 1;
+
+                                JsonObject obj = new JsonObject();
+
+                                obj.addProperty("source", "user_" + entry.getKey());
+                                obj.addProperty("target", "record_" + recid);
+
+                                result.add(obj);
+
+                                added.put(entry.getKey()+recid, true);
+
+                            }
+                        }
+                    }
+                }
+            }
+
+            System.out.println(count);
+
+
+            return result;
+
+        }
+
     }
 
     public Map<String, Double> getRecommendedRecordsForUser(String userID) {
@@ -109,49 +236,51 @@ public class WebInterface implements Runnable {
             List<Integer> downloads = new ArrayList<>();
             Set<Integer> users = new HashSet<>();
 
-            int depth = 4;
+            int depth = 2;
 
             String output = "";
             for (Path position : graphDb.traversalDescription()
                     .breadthFirst()
                     .relationships(NeoHelpers.RelTypes.DOWNLOADED)
-                    //.evaluator(Evaluators.fromDepth(3))
+                            //.evaluator(Evaluators.fromDepth(3))
                     .evaluator(Evaluators.toDepth(depth))
                     .traverse(user)) {
                 output += position + "\n";
 
-                for(Node node : position.nodes()) {
+                int c = 0;
 
-                    if(node.hasLabel(RECORDLABEL)) {
+                for (Node node : position.nodes()) {
+                    c += 1;
+                    if (node.hasLabel(RECORDLABEL)) {
                         String key = node.getProperty("id_bibrec").toString();
                         result.putIfAbsent(key, 0.0);
                         result.put(key, result.get(key) + getLevelScore(position.length(), depth));
 
                     }
 
-                    if(node.hasLabel(USERLABEL)) {
+                    if (node.hasLabel(USERLABEL)) {
                         users.add(Integer.parseInt(node.getProperty("id_user").toString()));
                     }
-
                 }
+
+                System.out.println(c);
+
             }
 
             double maxScore = 0.0;
 
-            for(String key : result.keySet()) {
+            for (String key : result.keySet()) {
                 result.put(key, Math.log(result.get(key)));
-                if(result.get(key) > maxScore) {
+                if (result.get(key) > maxScore) {
                     maxScore = result.get(key);
                 }
             }
 
-            for(String key : result.keySet()) {
-                result.put(key, result.get(key)/maxScore);
+            for (String key : result.keySet()) {
+                result.put(key, result.get(key) / maxScore);
             }
 
             System.out.println("Max score: " + maxScore);
-            System.out.println(result);
-            System.out.println(users);
             System.out.println("Number of users: " + users.size());
 
             //Collections.sort(downloads);
@@ -165,7 +294,20 @@ public class WebInterface implements Runnable {
         }
     }
 
+    private static void enableCORS(final String origin, final String methods, final String headers) {
+        before(new Filter() {
+            @Override
+            public void handle(Request request, Response response) {
+                response.header("Access-Control-Allow-Origin", origin);
+                response.header("Access-Control-Request-Method", methods);
+                response.header("Access-Control-Allow-Headers", headers);
+            }
+        });
+    }
+
     public void run() {
+
+        enableCORS("*", "*", "*");
 
         try {
             Thread.sleep(1000);
@@ -179,11 +321,17 @@ public class WebInterface implements Runnable {
         get("/recommended/:userid", "application/json", (request, response) ->
                 this.getRecommendedRecordsForUser(request.params(":userid")), new JsonTransformer());
 
+        get("/userrelations/:userid/:depth", "application/json", (request, response) ->
+                this.userRelationships(request.params(":userid"), request.params(":depth")), new JsonTransformer());
+
         get("/records", "application/json", (request, response) ->
                 this.getAllRecords(), new JsonTransformer());
 
         get("/relations", "application/json", (request, response) ->
                 this.getAllRelationships(), new JsonTransformer());
+
+        get("/relations-tuples/", "application/json", (request, response) ->
+                this.getAllRelationshipsTuples(), new JsonTransformer());
 
         get("/relations/:type", "application/json", (request, response) ->
                 this.getAllRelationships(request.params(":type")), new JsonTransformer());
